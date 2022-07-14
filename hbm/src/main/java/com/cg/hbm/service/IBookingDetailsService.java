d939e8f7e695aa48a777cf049d1beef5149d8978
AkshyS <akshysareshks@gmail.com>
2022-07-14 12:30:45 +0530

    Added AdminService and BookingDetails interface

---
+++ hbm/src/main/java/com/cg/hbm/service/IBookingDetailsService.java
@@ -0,0 +1,13 @@
+package com.cg.hbm.service;
+
+import com.cg.hbm.entities.BookingDetails;
+
+import java.util.List;
+
+public interface IBookingDetailsService {
+    public BookingDetails addBookingDetails(BookingDetails bookingDetails);
+    public BookingDetails updateBookingDetails(BookingDetails bookingDetails);
+    public BookingDetails removeBookingDetails(BookingDetails bookingDetails);
+    public List<BookingDetails> showAllBookingDetails();
+    public BookingDetails showBookingDetails(BookingDetails bookingDetails);
+}
